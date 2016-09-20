2c793600a686877844b474856eacf2ff7e7e8a86
“marcusngwj” <marcusngwj@msn.com>
2016-09-20 15:12:57 +0800

    Revert "Add FindByTagCommand class and add command to parser"
    This reverts commit b19599a6a1fb1da0071d89939155d50fd6c429e8.

--- src/seedu/addressbook/parser/Parser.java
+++ src/seedu/addressbook/parser/Parser.java
@@ -82,9 +82,6 @@ public class Parser {
 
             case ExitCommand.COMMAND_WORD:
                 return new ExitCommand();
-                
-            case FindByTagCommand.COMMAND_WORD:
-                return prepareFindByTag(arguments);
 
             case HelpCommand.COMMAND_WORD: // Fallthrough
             default:
@@ -230,23 +227,6 @@ public class Parser {
         final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
         return new FindCommand(keywordSet);
     }
-    
-    /**
-     * Parses arguments in the context of the find by tags command.
-     *
-     * @param args full command args string
-     * @return the prepared command
-     */
-    private Command prepareFindByTag(String args) {
-        final Matcher matcher = KEYWORDS_ARGS_FORMAT.matcher(args.trim());
-        if (!matcher.matches()) {
-            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    FindByTagCommand.MESSAGE_USAGE));
-        }
 
-        // keywords delimited by whitespace
-        final String[] keywords = matcher.group("keywords").split("\\s+");
-        final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
-        return new FindByTagCommand(keywordSet);
-    }
+
 }
